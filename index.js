6787026919d003a0f04dbc9f15cc3a21a79d332e
srujanchinta8 <42657821+srujanchinta8@users.noreply.github.com>
2019-10-17 18:51:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const demo = async () => {
   status('');
 
   // Make a prediction through the locally hosted cat.jpg.
-  const catElement = document.getElementById('cat');
+  const catElement = document.getElementById('lion');
   if (catElement.complete && catElement.naturalHeight !== 0) {
     predict(catElement);
     catElement.style.display = '';
